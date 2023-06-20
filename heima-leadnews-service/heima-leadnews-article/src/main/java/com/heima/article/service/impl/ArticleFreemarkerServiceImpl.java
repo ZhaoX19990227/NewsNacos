d4bd58cbccecc5a1f14dafc21be16e8a49584ce2
zhaoxiang <1084691005@qq.com>
2023-06-20 09:44:10 +0800

    first commit

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleFreemarkerServiceImpl.java
@@ -16,7 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -46,6 +46,7 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
 
     /**
      * 生成静态文件上传到minIO中
+     *
      * @param apArticle
      * @param content
      */
@@ -54,17 +55,17 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
     public void buildArticleToMinIO(ApArticle apArticle, String content) {
         //已知文章的id
         //4.1 获取文章内容
-        if(StringUtils.isNotBlank(content)){
+        if (StringUtils.isNotBlank(content)) {
             //4.2 文章内容通过freemarker生成html文件
             Template template = null;
             StringWriter out = new StringWriter();
             try {
                 template = configuration.getTemplate("article.ftl");
                 //数据模型
-                Map<String,Object> contentDataModel = new HashMap<>();
+                Map<String, Object> contentDataModel = new HashMap<>();
                 contentDataModel.put("content", JSONArray.parseArray(content));
                 //合成
-                template.process(contentDataModel,out);
+                template.process(contentDataModel, out);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -76,31 +77,32 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
 
 
             //4.4 修改ap_article表，保存static_url字段
-            apArticleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId,apArticle.getId())
-                    .set(ApArticle::getStaticUrl,path));
+            apArticleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId, apArticle.getId())
+                    .set(ApArticle::getStaticUrl, path));
 
             //发送消息，创建索引
-          //  createArticleESIndex(apArticle,content,path);
+            createArticleESIndex(apArticle, content, path);
 
         }
     }
 
-   // @Autowired
-    //private KafkaTemplate<String,String> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     /**
      * 送消息，创建索引
+     *
      * @param apArticle
      * @param content
      * @param path
      */
-//    private void createArticleESIndex(ApArticle apArticle, String content, String path) {
-//        SearchArticleVo vo = new SearchArticleVo();
-//        BeanUtils.copyProperties(apArticle,vo);
-//        vo.setContent(content);
-//        vo.setStaticUrl(path);
-//
-//        //kafkaTemplate.send(ArticleConstants.ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(vo));
-//    }
+    private void createArticleESIndex(ApArticle apArticle, String content, String path) {
+        SearchArticleVo vo = new SearchArticleVo();
+        BeanUtils.copyProperties(apArticle, vo);
+        vo.setContent(content);
+        vo.setStaticUrl(path);
+
+        kafkaTemplate.send(ArticleConstants.ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(vo));
+    }
 
 }
--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/listener/SyncArticleListener.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/listener/SyncArticleListener.java
@@ -10,7 +10,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -22,11 +22,11 @@ public class SyncArticleListener {
     @Autowired
     private RestHighLevelClient restHighLevelClient;
 
-   // @KafkaListener(topics = ArticleConstants.ARTICLE_ES_SYNC_TOPIC)
-    public void onMessage(String message){
-        if(StringUtils.isNotBlank(message)){
+    @KafkaListener(topics = ArticleConstants.ARTICLE_ES_SYNC_TOPIC)
+    public void onMessage(String message) {
+        if (StringUtils.isNotBlank(message)) {
 
-            log.info("SyncArticleListener,message={}",message);
+            log.info("SyncArticleListener,message={}", message);
 
             SearchArticleVo searchArticleVo = JSON.parseObject(message, SearchArticleVo.class);
             IndexRequest indexRequest = new IndexRequest("app_info_article");
@@ -36,7 +36,7 @@ public class SyncArticleListener {
                 restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
             } catch (IOException e) {
                 e.printStackTrace();
-                log.error("sync es error={}",e);
+                log.error("sync es error={}", e);
             }
         }
 
